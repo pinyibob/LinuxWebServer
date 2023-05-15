1286a8e67bacab741f66436ce0afc1d07981629b
pinyibob <812837626@qq.com>
2023-05-15 21:50:06 +0800

    界面逻辑舍弃保留文章信息；新增动态html slogan

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     //需要修改的数据库信息,登录名,密码,库名
     string user = "root";
     string passwd = "123456";
-    string databasename = "chatgpt_service";
+    string databasename = "pinyibob_blog";
 
     //命令行解析
     Config config;
